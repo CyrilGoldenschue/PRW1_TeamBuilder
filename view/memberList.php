f3554321cfd0d571af1a131f315705d11f2954b4
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2021-10-12 14:23:55 +0200

    Add data on member list page

--- view/memberList.php
+++ view/memberList.php
@@ -5,24 +5,36 @@ ob_start();
     <section class="row">
         <table class="table">
             <thead>
-                <tr>
-                    <td>
-                        Member
-                    </td>
-                    <td>
-                        Teams
-                    </td>
-                </tr>
+            <tr>
+                <td>
+                    Member
+                </td>
+                <td>
+                    Teams
+                </td>
+            </tr>
             </thead>
             <tbody>
+            <?php foreach ($members as $member) { ?>
                 <tr>
                     <td>
-
+                        <?= $member->name ?>
                     </td>
                     <td>
+                        <?php $teams = $member->teams();
+                        $lastElement = end($teams);;
+                        foreach ($teams as $team) { ?>
+                            <?= $team->name ?>
+                            <?php if ($team->name != $lastElement->name) { ?>
 
+                                <?= ", " ?>
+
+                            <?php } ?>
+                        <?php } ?>
                     </td>
                 </tr>
+
+            <?php } ?>
             </tbody>
 
         </table>
