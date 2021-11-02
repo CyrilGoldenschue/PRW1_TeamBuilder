2b3abdcc466112f2e4db21e6f826ba07eeab309c
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2021-11-02 12:02:08 +0100

    Add method to display create team page

--- controller/TeamController.php
+++ controller/TeamController.php
@@ -2,6 +2,7 @@
 namespace TeamBuilder\Controller;
 require "vendor/autoload.php";
 
+use TeamBuilder\Model\Member;
 use TeamBuilder\Model\Team;
 
 class TeamController
@@ -9,6 +10,15 @@ class TeamController
     public function teamInfo(){
         $team = Team::find($_GET['id']);
         require "./view/teamInfo.php";
+    }
+
+    public function createTeam(){
+        require "./view/createTeam.php";
+    }
+
+    public function create(){
+        $member = Member::find($_SESSION['user_connected']->id);
+        require "./view/myTeamList.php";
     }
 
 
