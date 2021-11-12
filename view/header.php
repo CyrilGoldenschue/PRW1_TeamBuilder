acca823b9b721a1d6ce14f11a8a2d0ac51096c94
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2021-11-12 12:17:39 +0100

    End exam

--- view/header.php
+++ view/header.php
@@ -4,7 +4,7 @@ ob_start();
     <header class="home">
         <section class="container">
             <h1><a class="title_home" href="?action=home">TeamBuilder</a></h1>
-            <a>Version: Début Examen - Cyril</a><br>
+            <a>Version: Fin Examen - Cyril</a><br>
             <a>Connecté en tant que :</a> <a class="link" href="?action=MyProfile"><?= $_SESSION['user_connected']->name ?></a>
         </section>
     </header>
