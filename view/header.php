ba164a3e37d410c303e979f78197cf48bb4c1224
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2021-11-12 10:01:01 +0100

    Start exam by add name in header

--- view/header.php
+++ view/header.php
@@ -4,6 +4,7 @@ ob_start();
     <header class="home">
         <section class="container">
             <h1><a class="title_home" href="?action=home">TeamBuilder</a></h1>
+            <a>Version: Début Examen - Cyril</a><br>
             <a>Connecté en tant que :</a> <?= $_SESSION['user_connected']->name ?>
         </section>
     </header>
