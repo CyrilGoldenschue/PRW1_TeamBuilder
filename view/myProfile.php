eda173e6302ec83ffa9fdcf52eb4afc30406d0a8
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2021-11-12 11:29:25 +0100

    Add displaying the team for the profile member

--- view/myProfile.php
+++ view/myProfile.php
@@ -3,12 +3,21 @@ ob_start();
 ?>
 
     <section class="row">
-        <a>nom : <?= $member->name ?> </a>
-        <a>role : <?= $member->role->name ?> </a>
-        <a>statu : <?= $member->statu->name ?> </a>
+        <div class="column">
+            <a>nom : <?= $member->name ?> </a>
+            <a>role : <?= $member->role->name ?> </a>
+            <a>statu : <?= $member->statu->name ?> </a>
+        </div>
+        <div class="column">
+            <?php foreach ($member->teams() as $team) : ?>
+                <?php foreach ($team->members() as $memberTeam): ?>
 
 
-
+                    <?= ($memberTeam->is_captain == 1 && $member->id == $memberTeam->id) ? "<a> Capitaine de : </a><br>" . $team->name : "" ?>
+                    <?= ($memberTeam->is_captain == 0 && $member->id == $memberTeam->id) ? "<a> Membre de : </a><br>" . $team->name : "" ?>
+                <?php endforeach; ?>
+            <?php endforeach; ?>
+        </div>
     </section>
 
 
