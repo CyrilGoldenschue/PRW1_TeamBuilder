1d787eff5495b6769c203f962bf5618bdef591d2
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2021-11-12 12:02:14 +0100

    Add methode to editProfile

--- controller/MemberController.php
+++ controller/MemberController.php
@@ -3,6 +3,8 @@ namespace TeamBuilder\Controller;
 require "vendor/autoload.php";
 
 use TeamBuilder\Model\Member;
+use TeamBuilder\Model\Role;
+use TeamBuilder\Model\Status;
 
 class MemberController
 {
@@ -21,9 +23,17 @@ class MemberController
         $member = Member::find($_SESSION['user_connected']->id);
         require "./view/myProfile.php";
     }
+
     public function profile(){
         $member = Member::find($_GET['id']);
         require "./view/myProfile.php";
+    }
+
+    public function editProfile(){
+        $member = Member::find($_GET['id']);
+        $roles = Role::all();
+        $status = Status::all();
+        require "./view/editProfile.php";
     }
 
     public function listModo(){
